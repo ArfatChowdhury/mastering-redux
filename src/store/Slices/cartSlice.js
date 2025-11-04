7d0f9680c5555d4a0abd615d4db5232ff34aad60
Arfat Chowdhury <arfatahsan60@gmail.com>
2025-11-04 18:47:15 +0600

    creating shopping cart in existing app for understand the complexity

---
+++ src/store/Slices/cartSlice.js
@@ -0,0 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: [{
+        items: [],
+        total: 0
+    }],
+
+    reducers:{
+
+        addToCart: (state, action) =>{
+            const {productId} = action.payload
+            const existingProduct = state.items.find(item => item.productId === productId)
+
+            console.log('existingProduct', existingProduct);
+            
+            if(existingProduct){
+                existingProduct.quantity +=1
+            }else{
+                state.items.push({
+                    id: Date.now().toString(),
+                    productId: productId,
+                    quantity: 1
+                })
+            }
+        },
+        removeItem: (state, action) =>{
+            const {itemId} = action.payload
+            state.items = state.items.filter(item => item.id !== itemId)
+        },
+        
+        updateQuantity: (state, action)=>{
+            const {itemId, quantity} = action.payload
+            const item = state.items.find(item => item.id === itemId)
+
+            if (item) {
+                item.quantity = quantity
+            }
+        },
+        clearCart: (state) => {
+            state.items = [];
+        }
+    }
+    
+})
+
+export const { addToCart, removeFromCart, updateQuantity, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from '../slices/todoSlice'
+import productReducer from './Slices/ProductSlice'
+import cartReducer from './Slices/cartSlice'
 
 export const store = configureStore({
     reducer:{
-        todos: todoReducer
+        todos: todoReducer,
+        products: productReducer,
+        cart: cartReducer
     }
 })
